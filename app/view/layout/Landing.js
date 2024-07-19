ebf7f36541fd71925b74b29b2b7b045ca7ffb0c8
elsimetchami <elsimetchami@gmail.com>
2024-07-19 16:39:42 +0200

    menu_account

--- app/view/layout/Landing.js
+++ app/view/layout/Landing.js
@@ -4,9 +4,67 @@
 Ext.define('Compare.view.layout.Landing', {
     extend: 'Ext.panel.Panel',
     alias: 'widget.layout.landing',
-    title: 'Welcome!',
+    // title: 'Welcome!',
+    title:{
+        items:[
+            {
+                xtype:'button',
+                // text:'hello',
+                style: 'width: 60px;height:60px;background-image:url(resources/images/picture.jpeg) !important; background-size:cover;',
+                
+            }
+            
+        ]
+
+    },
     bodyPadding: 10,
     html: 'Welcome to the Car Tracker Administration Site!',
+    items:[
+        {
+            xtype:'button',
+            text:'hello',
+            style: 'width: 60px;height:60px;background-image:url(resources/images/picture.jpeg) !important; background-size:cover;',
+            
+        }
+    ],
+    // {
+    //     xtype:'button',
+    //     // text:'hello',
+    //     style: 'width: 60px;height:60px;background-image:url(resources/images/picture.jpeg) !important; background-size:cover;',
+        
+    // },
+    Header:{
+        items:[
+            {
+                xtype:'button',
+                text:'hello',
+                style: 'width: 60px;height:60px;background-image:url(resources/images/picture.jpeg) !important; background-size:cover;',
+                listeners: {
+                    render: function () {
+                        console.log(this);
+                        this.getEl().on('onclick', function () {
+                            var menut= Ext.create('Ext.menu.Menu', {
+                                        width: 100,
+                                        plain: true,
+                                        autoDestroy:true,
+                                        floating: false,  // usually you want this set to True (default)
+                                        renderTo: Ext.getBody(),  // usually rendered by it's containing component
+                                        items: [{
+                                            text: 'plain item 1'
+                                        },{
+                                            text: 'plain item 2'
+                                        },{
+                                            text: 'plain item 3'
+                                        }]
+                                    });
+                                    // menut.showAt( this.getXY() )
+                        }
+                    )}
+                }
+                // showAt( e.getXY() )
+            }
+        ]
+    },
     initComponent: function(){
         var me = this;
         Ext.applyIf(me,{
--- app/view/layout/North.js
+++ app/view/layout/North.js
@@ -5,12 +5,51 @@ Ext.define('Compare.view.layout.North', {
     extend: 'Ext.panel.Panel',
     alias: 'widget.layout.north',
     region: 'north',
-    bodyPadding: 5,
-    html: '<img src="resources/images/comparable.png" /><h1>Comparable SARL</h1>',
-    cls: 'header',                  
+    // bodyPadding: 5,
+    // html: '<img src="resources/images/comparable.png" /><h1>Comparable SARL</h1>',
+    cls: 'header',    
+    maxHeight: 70,
+    // html:'<div><img src="resources/images/picture.jpeg" /></div>',
+    
+    
+    // html:'<div><img src="resources/images/comparable.png" /></div>',       
     initComponent: function(){
         var me = this;
         Ext.applyIf(me,{
+            items:[
+                {
+                    xtype:'button',
+                    text:'hello',
+                    style: 'width: 60px;height:60px;background-image:url(resources/images/picture.jpeg) !important; background-size:cover;',
+                    listeners: {
+                        render: function (e) {
+                            // showAt( this.getXY() )
+                            console.log(this);
+                            this.on('click', function () {
+                               var menu= Ext.create('Ext.menu.Menu', {
+                                    width: 100,
+                                    plain: true,
+                                    autoDestroy:true,
+                                    floating: false,  // usually you want this set to True (default)
+                                    renderTo: Ext.getBody(),  // usually rendered by it's containing component
+                                    items: [{
+                                        text: 'plain item 1'
+                                    },{
+                                        text: 'plain item 2'
+                                    },{
+                                        text: 'plain item 3'
+                                    }]
+                                });
+                                menu.showAt( e.getXY() )
+                                
+                            // alert('test');
+                                //logic of whatever you want happening...
+                            });
+                        }
+                    },
+                    showDelay: 10,
+                }
+            ]
             
         });
         me.callParent( arguments );
